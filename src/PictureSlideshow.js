ea9dc2c445800e9d57084d5475d1994320bf9284
Jack Syvertsen <jmsyvert@stud.ntnu.no>
2018-09-18 19:58:34 +0200

    Added handlers to iterate through music for different picture selections.

--- src/PictureSlideshow.js
+++ src/PictureSlideshow.js
@@ -1,19 +1,36 @@
 import ReactDOM, { render } from 'react-dom';
 import React, { Component } from 'react';
 import './App/App.css';
+import AudioPlayer from './AudioPlayer.js';
 
 class ImageSlider extends React.Component {
   
   constructor(){
     super();
     this._handleClick = this._handleClick.bind(this);
-    this.state = {firstImg : ''};
+    this.state = {firstImg : '', src : 'https://www.mfiles.co.uk/mp3-downloads/book1-fugue02.mp3', count : 0};
   }
   _handleClick(e){
-    this.setState({firstImg : e.target.src});
+    if (this.state.count == 0){
+      this.setState({firstImg : e.target.src, src : 'https://www.mfiles.co.uk/mp3-downloads/bach-bourree-in-e-minor-guitar.mp3', count : this.state.count + 1});
+    }
+    else if (this.state.count == 1){
+      this.setState({firstImg : e.target.src, src : 'https://www.mfiles.co.uk/mp3-downloads/book1-fugue02.mp3', count : this.state.count + 1});
+    }
+    else if (this.state.count == 2){
+      this.setState({firstImg : e.target.src, src : 'https://www.mfiles.co.uk/mp3-downloads/francisco-tarrega-lagrima.mp3', count : this.state.count + 1});
+    }
+    else{
+      this.setState({firstImg : e.target.src, src : 'https://www.mfiles.co.uk/mp3-downloads/chopin-tarrega-nocturne-op9-no2-guitar.mp3', count : 0});
+    }
   }
+  
   render() {
     var mainImg = this.state.firstImg ? this.state.firstImg : this.props.images[0];
+    const audioPath = this.state.src;
+    let audio;
+    audio = <AudioPlayer src = {audioPath}/>
+    
     return(
       <div>
         <div className="col-12" maxHeight="100px">
@@ -21,14 +38,17 @@ class ImageSlider extends React.Component {
 
         </div>
         <div className="col-12" maxHeight="50px">
-          {this.props.images.map((image, key) => {
+          {this.props.images.map((image, id) => {
             return(
               <img
-                key={key}
+                key={id}
                 className ={mainImg==image? "selectedThumbnail" :"allThumbnail"}
                 src={image}
                 onClick={this._handleClick} />
             )})}
+        </div>
+        <div className="AudioPlayer">
+              { audio }
         </div>
         
       </div>
@@ -41,10 +61,10 @@ class ImageSlideShow extends React.Component {
       super(props, context);
       this.state = {};
       this.state.images= [];
-      this.state.images = ['https://www.shareicon.net/data/512x512/2016/11/30/859203_nature_512x512.png',
-      'https://www.shareicon.net/data/512x512/2016/11/16/854281_nature_512x512.png',
-      'https://www.shareicon.net/data/512x512/2016/11/16/854313_flower_512x512.png',
-    'https://www.shareicon.net/data/512x512/2016/11/16/854312_nature_512x512.png'];
+      this.state.images = [('https://www.shareicon.net/data/512x512/2016/11/30/859203_nature_512x512.png'),
+      ('https://www.shareicon.net/data/512x512/2016/11/16/854281_nature_512x512.png'),
+      ('https://www.shareicon.net/data/512x512/2016/11/16/854313_flower_512x512.png'),
+    ('https://www.shareicon.net/data/512x512/2016/11/16/854312_nature_512x512.png')];
       }
   
 render(){
