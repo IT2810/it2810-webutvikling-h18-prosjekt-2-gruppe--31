6b7a4b17ad87cf9b5ce933006b96bed679c75468
katrilh <katrilh@stud.ntnu.no>
2018-09-19 16:13:05 +0200

    Working text show #4

--- src/Words.js
+++ src/Words.js
@@ -1,63 +1,120 @@
-import React, { Component} from 'react';
+import React, { Component } from 'react';
+// import * as poem from '..\\public\\words\\poem\\1.json';
 
 
 class Words extends Component {
-  
-    constructor(props) {
-      super(props);
-      this.state = {
-        title : "default title",
-        type : "default type",
-        author : "default author",
-        content : "default content",
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "default title",
+      type: "default type",
+      author: "default author",
+      content: "default content",
+      isLoading: false,
+      error: null,
+    }
+    // this.loadJSON = this.loadJSON.bind(this);
+    // this.updateContent = this.updateContent.bind(this);
+  }
+
+  /*
+  loadJSON(filepath, callback) {
+    alert(filepath);
+    let rawFile = new XMLHttpRequest();
+    rawFile.overrideMimeType("application/json");
+    rawFile.open("GET", filepath, true);
+    rawFile.onreadystatechange = function () {
+      if (rawFile.readyState === 4 && rawFile.status == "200") {
+        callback(rawFile.responseText);
       }
-      this.loadJSON = this.loadJSON.bind(this);
-      this.updateContent = this.updateContent.bind(this);
     }
+    rawFile.send(null); 
+  }
+*/
 
-    loadJSON(file, callback) {
-        let rawFile = new XMLHttpRequest();
-        rawFile.overrideMimeType("application/json");
-        rawFile.open("GET", file, true);
-        rawFile.onreadystatechange = function() {
-            if (rawFile.readyState === 4 && rawFile.status == "200") {
-                callback(rawFile.responseText);
-            }
+/*
+  updateContent(path) {
+    this.loadJSON(path, function (response) {
+      alert(response);
+      let filecontent = JSON.parse(response);
+      alert(filecontent);
+
+      this.setState({
+        title: filecontent.title,
+        type: filecontent.type,
+        author: filecontent.author,
+        content: filecontent.content,
+      });
+    })
+  }
+  */
+
+  // _handleEvent(e) {
+  //   this.updateContent(e.target.src); // or howeer to get the path
+  // }
+
+  componentDidMount() {
+    //this.setState({ isLoading: true });
+
+    fetch('/words/poem/1.json')
+      .then(response => {
+        if (response.ok) {
+          response.json()
+          .then(data => this.setState({
+            title: data.title,
+            type: data.type,
+            author: data.author,
+            content: data.content,
+          }))
+        } else {
+          throw new Error('Something went wrong ...');
         }
-        rawFile.send(null);
+      })
+      // .then(data => this.setState({ hits: data.hits, isLoading: false }))
+      .catch(error => this.setState({ error, isLoading: false }));   
+  }
+
+
+  
+  render() {
+
+    const { title, type, author, contenct, isLoading, error } = this.state;
+
+    if (error) {
+      return <p>{error.message}</p>;
     }
 
-    updateContent(path){
-        this.loadJSON(path, function(response) {
-            let filecontent = JSON.parse(response);
-
-            this.setState({
-                title : filecontent.title,
-                type : filecontent.type,
-                author : filecontent.author,
-                content : filecontent.content,
-              });        
-        })
+    if (isLoading) {
+      return <p>Loading ...</p>;
     }
-
- 
-    render() {
-      return(
-          
-        <div>        
+    
+    return (
+      <div className="TextContainer">
         {this.state.title}
         <br></br>
-        {this.state.type}
+        {/* {this.state.type} */}
         <br></br>
         {this.state.author}
         <br></br>
         {this.state.content}
-        </div>
-      )
-    }
-  
+        <br></br>
+        {/* <button onClick={(e) => this.updateContent("./poem/2.json", e)}>update</button> */}
+      </div>
+    )
   }
-  
-  export default Words;
+}
 
+export default Words;
+// export { updateContent };
+
+  // export function updatePath(path){
+  //   this.setState({
+  //     title : "Ny Tittel"});
+  // 
+  //     return null; 
+  //   };
+
+
+  //  
 // https://codepen.io/KryptoniteDove/post/load-json-file-locally-using-pure-javascript
