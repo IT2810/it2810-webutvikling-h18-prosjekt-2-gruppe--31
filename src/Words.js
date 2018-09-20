70de37dc3102221a66fbc1aeb203d538e625e16e
katrilh <katrilh@stud.ntnu.no>
2018-09-20 13:47:24 +0200

    done with styling, standalone component done #24 #4

--- src/Words.js
+++ src/Words.js
@@ -5,64 +5,55 @@ class Words extends Component {
 
   constructor(props) {
     super(props);
+
+    this.words_db = {
+      'Poem': ['/words/Poem/1.json', '/words/Poem/2.json', '/words/Poem/3.json', '/words/Poem/4.json'],
+      'Humour': ['/words/Humour/1.json', '/words/Humour/2.json', '/words/Humour/3.json', '/words/Humour/4.json'],
+      'Wisdom': ['/words/Wisdom/1.json', '/words/Wisdom/2.json', '/words/Wisdom/3.json', '/words/Wisdom/4.json']
+    };
+
     this.state = {
       title: "default title",
       type: "default type",
       author: "default author",
       content: "default content",
-      isLoading: false,
       error: null,
     }
+    this.updateWords = this.updateWords.bind(this);
   }
 
-
-
-  componentDidMount() {
-    //this.setState({ isLoading: true });
-
-    fetch('/words/Poem/3.json')
+  updateWords(category, index) {
+    // let paths = this.words_db[category];
+    let currentPath = this.words_db[category][parseInt(index)];
+    fetch(currentPath)
       .then(response => {
         if (response.ok) {
           response.json()
-          .then(data => this.setState({
-            title: data.title,
-            type: data.type,
-            author: data.author,
-            content: data.content,
-          }))
+            .then(data => this.setState({
+              title: data.title,
+              type: data.type,
+              author: data.author,
+              content: data.content,
+            }))
         } else {
           throw new Error('Something went wrong ...');
         }
       })
-      // .then(data => this.setState({ hits: data.hits, isLoading: false }))
-      .catch(error => this.setState({ error, isLoading: false }));   
-  }
+      .catch(error => this.setState({ error }));
+  };
 
 
-  
   render() {
 
     return (
-      <div className="wordContainer"> {this.state.title
-        
-      }
-         <div className="wordTitle" > {this.state.title} </div>
-         <div className="wordAuthor" > {this.state.author} </div>
-         <div className="wordContent" > {this.state.content} </div>
- 
-        {/* <button onClick={(e) => this.updateContent("./poem/2.json", e)}>update</button> */}
-        {/* this.state.content.split('\n').map((item, i) => {
-                        return key={i}> {item} </p>;
-                    }) */}
+      <div className="wordContainer">
+        {this.updateWords(this.props.cat, this.props.index)}
+        <div className="wordTitle" > {this.state.title} </div>
+        <div className="wordAuthor" > {this.state.author} </div>
+        <div className="wordContent" > {this.state.content} </div>
       </div>
     )
   }
 }
 
 export default Words;
-//
-
-/*
-
-
-)*/
